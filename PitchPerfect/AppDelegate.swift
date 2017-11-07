dd5910fd4562b90f8ddf77fcb5860018acb716bd
Olutosin Oduwole <olutosin@olutosins-mbp.corp.capgemini.com>
2017-11-07 17:23:59 +0100

    chapter two completed

--- PitchPerfect/AppDelegate.swift
+++ PitchPerfect/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         // Override point for customization after application launch.
-        print("Hey you :)");
+        print("Hey you : didFinishLaunchingWithOptions() was called :)");
         
         return true
     }
@@ -43,7 +43,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!;
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        
+        print(getDateNow());
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print(getDateNow());
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: UIButton) {
+        //print("recor button was pressed")
+        
+        //
+        recordingLabel.text = "Recording in progress...";
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        //
+        recordingLabel.text = "Recording stopped.";
+    }
+    
+    private func getDateNow() -> String {
+        
+        let date = Date();
+        let calendar = Calendar.current;
+        
+        let components = calendar.dateComponents([.year, .month, .day,.hour, .minute,.second, .nanosecond], from: date);
+        
+        let year = components.year
+        let month = components.month
+        let day = components.day
+        let hour = components.hour
+        let minute = components.minute
+        let second = components.second
+        let nSecond = components.nanosecond
+        
+        let dateNow = String(year!) + "-" + String(month!) + "-" + String(day!) + " [" + String(hour!)  + ":" + String(minute!) + ":" +  String(second!) +  ":" +  String(nSecond!) + "]";
+        
+        return dateNow;
+    }
 }
 
